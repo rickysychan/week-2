91b86307455f6a27b48b88ed1b08615c8cada5bb
Ricky Chan <rickysychan@gmail.com>
2017-09-11 17:42:12 +0000

    completed

---
+++ w2d1/Scripting-HTTP-Requests-with-the-Request-Package/main.js
@@ -0,0 +1,18 @@
+console.log('Downloading image...');
+
+var request = require('request');
+var fs = require('fs');
+
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response message: ', response.statusMessage);
+         console.log('Content type: ', response.headers['content-type']);
+         console.log('Download complete.');
+       })
+       .pipe(fs.createWriteStream('./future.jpg'))
+
